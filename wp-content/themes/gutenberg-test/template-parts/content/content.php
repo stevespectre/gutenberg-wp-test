808070f2e55a75072c105436da8471f3fa9da833
moon42_istvanerdos <istvan.erdos@moon42.com>
2019-10-09 09:57:13 +0200

    Remove some unnecessary lines of code

--- wp-content/themes/gutenberg-test/template-parts/content/content.php
+++ wp-content/themes/gutenberg-test/template-parts/content/content.php
@@ -1,20 +1,9 @@
 <article>
   <header class="entry-header">
-  <?php
-		the_content();
-  ?>
+    <?php
+      the_content();
+    ?>
 
-  <hr>
-
-  <?php
-  if ( ! function_exists( '_wp_render_title_tag' ) ) {
-    function theme_slug_render_title() {
-  ?>
-  <title><?php wp_title( '|', true, 'right' ); ?></title>
-  <?php
-    }
-    add_action( 'wp_head', 'theme_slug_render_title' );
-  }
-  ?>
-  </header><!-- .entry-header -->
+    <hr>
+  </header>
 </article>
